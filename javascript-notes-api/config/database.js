f3b6f4adc709cc0f0c715e64a55d06600e8e0c7e
joaolavelino <joaolavelino@gmail.com>
2022-05-11 17:35:22 -0300

    _FIRST DEPLOY

--- javascript-notes-api/config/database.js
+++ javascript-notes-api/config/database.js
@@ -2,9 +2,19 @@
 const mongoose = require("mongoose");
 //2 - Connect mongoose with the global promise
 mongoose.Promise = global.Promise;
-//3 - Connect with the DB
+//DEPLOY - require the environment
+require("dotenv").config();
+const MONGO_URL = process.env.MONGO_URL;
+
+//3 - Connect with the DB (commented on the deploy stage)
 mongoose
-  .connect("mongodb://localhost/javascriptnote", {
+  // .connect("mongodb://localhost/javascriptnote", {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true,
+  //   // useCreateIndex: true, THIS IS NO LONGER SUPPORTED - it's automatically set to true
+  // })
+  //DEPLOY - change the adress of the database to the actual adress on the cloud
+  .connect(MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     // useCreateIndex: true, THIS IS NO LONGER SUPPORTED - it's automatically set to true
